7344b55dc4fc65013455e404525203c64cab2db4
Israel Lavi <israel.lavi@intl.att.com>
2018-05-10 15:33:30 +0300

    First commit

---
+++ mongod.bat
@@ -0,0 +1,5 @@
+REM Israel
+START "" "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --dbpath "C:\dev\github\martin\LogicalCommandBackEnd\data"
+
+REM Martin
+START "" "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --dbpath "C:\dev\github\martin\LogicalCommandBackEnd\data"
